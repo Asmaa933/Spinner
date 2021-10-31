6cd0aa3d8603ae9dc37e5ea6728d7a9f063520ec
Asmaa <asmaatarek93@gmail.com>
2021-11-01 01:03:57 +0200

    Remove Posts Api

--- Spinner/Features/Home/HomeViewController.swift
+++ Spinner/Features/Home/HomeViewController.swift
@@ -11,33 +11,15 @@ class HomeViewController: UIViewController {
         
     //MARK: - Variables
     private var spinner = SpinnerView()
-    private var postsArray = [Post]() {
-        didSet {
-            removeSpinner()
-        }
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        getPosts()
+
     }
 }
 
 //MARK: - Helper Methods
 private extension HomeViewController {
-    
-    func getPosts() {
-        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
-        showSpinner()
-        URLSession.shared.dataTask(with: url) {[weak self] data, response, error in
-            if data != nil && error == nil {
-                let posts = try? JSONDecoder().decode([Post].self, from: data!)
-                self?.postsArray = posts ?? []
-            } else {
-                self?.showError(error: error?.localizedDescription.description)
-            }
-        }.resume()
-    }
     
     func showSpinner() {
         spinner = SpinnerView(frame: CGRect(x: view.frame.width / 2, y: view.frame.height / 2, width: 100, height: 100))
@@ -51,10 +33,5 @@ private extension HomeViewController {
             self?.spinner.removeSpinner()
             self?.view.isUserInteractionEnabled = true
         }
-    }
-    
-    func showError(error: String?) {
-        removeSpinner()
-        print(error ?? "")
     }
 }
