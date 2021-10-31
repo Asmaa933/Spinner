901ae0df131a7980e219565686a2156b24b5c9c7
Asmaa Tarek <asmaatarek93@gmail.com>
2021-11-01 01:09:21 +0200

    Replace api call with asyncAfter (#5)
    * Remove postsTableView
    
    * Remove Posts Api
    
    * Add asyncAfter as a simulate for api call

--- Spinner/Features/Home/HomeViewController.swift
+++ Spinner/Features/Home/HomeViewController.swift
@@ -8,86 +8,40 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-    
-    //MARK: - Outlets
-    @IBOutlet private weak var tableView: UITableView!
-    
+        
     //MARK: - Variables
     private var spinner = SpinnerView()
-    private var postsArray = [Post]() {
-        didSet {
-            removeSpinner()
-            reloadTableView()
-        }
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        getPosts()
+        setupView()
     }
 }
 
 //MARK: - Helper Methods
 private extension HomeViewController {
     
-    func getPosts() {
-        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+    func setupView() {
         showSpinner()
-        URLSession.shared.dataTask(with: url) {[weak self] data, response, error in
-            if data != nil && error == nil {
-                let posts = try? JSONDecoder().decode([Post].self, from: data!)
-                self?.postsArray = posts ?? []
-            } else {
-                self?.showError(error: error?.localizedDescription.description)
-            }
-        }.resume()
+        
+        /// wait 3 seconds and execute removeSpinner
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {[weak self] in
+            self?.removeSpinner()
+        }
     }
     
     func showSpinner() {
         spinner = SpinnerView(frame: CGRect(x: view.frame.width / 2, y: view.frame.height / 2, width: 100, height: 100))
         spinner.center = view.center
-        tableView.isUserInteractionEnabled = false
-        tableView.addSubview(spinner)
+        view.isUserInteractionEnabled = false
+        view.addSubview(spinner)
     }
     
     func removeSpinner() {
         DispatchQueue.main.async {[weak self] in
-            self?.spinner.removeSpinner()
-            self?.tableView.isUserInteractionEnabled = true
+            guard let self = self else { return }
+            self.spinner.removeSpinner()
+            self.view.isUserInteractionEnabled = true
         }
     }
-    
-    func reloadTableView() {
-        DispatchQueue.main.async {[weak self] in
-            self?.tableView.reloadData()
-        }
-    }
-    
-    func showError(error: String?) {
-        removeSpinner()
-        print(error ?? "")
-    }
-}
-
-//MARK: - UITableViewDelegate Imp
-extension HomeViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(postsArray[indexPath.row].title ?? "")
-    }
-}
-
-//MARK: - UITableViewDataSource Imp
-
-extension HomeViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return postsArray.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = postsArray[indexPath.row].title
-      //  cell.detailTextLabel?.text = postsArray[indexPath.row].body
-        return cell
-    }
-    
 }
